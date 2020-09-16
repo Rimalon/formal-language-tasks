d9cdc269ca1cd15e302b8a51ccadb9bfb7646456
Rimalon <rimalon@yandex.ru>
2020-09-16 19:08:10 +0300

    Fix args in test

--- src/tests/graph_test.py
+++ src/tests/graph_test.py
@@ -1,27 +1,21 @@
 import unittest
 
-from pyformlang.regular_expression import Regex
 from src.classes import Graph
 
 test_resources_path = '/home/travis/build/Rimalon/formal-language-tasks/src/tests/resources/'
 
 
 class GraphTestCase(unittest.TestCase):
-    def test_from_regex(self):
-        graph = Graph.from_regex_file(test_resources_path + 'regex_abc.txt')
-        for label, matrix in graph.label_matrices.items():
-            for i in range(matrix.nrows):
-                for j in range(matrix.ncols):
-                    print(i, ' ', j, ' ', label, ' ', matrix[i, j])
-                    #self.assertEqual((i == 0 and j == 1 and label == 'a') or (i == 0 and j == 2 and label == 'b'), matrix[i, j])
-
     def test_from_file(self):
         graph = Graph.from_file(test_resources_path + 'graph.txt')
-        for label, matrix in graph.label_matrices.items():
-            for i in range(matrix.nrows):
-                for j in range(matrix.ncols):
-                    print(i, ' ', j, ' ', label, ' ', matrix[i, j])
-                    #self.assertEqual((i == 0 and j == 1 and label == 'b') or (i == 0 and j == 2 and label == 'a'), matrix[i, j])
+        self.assertTrue(graph['a'][0, 1])
+        self.assertTrue(graph['b'][1, 2])
+        self.assertTrue(graph['c'][2, 3])
+        self.assertTrue(graph['c'][0, 1])
+        self.assertTrue(graph['b'][1, 3])
+        self.assertFalse(graph['a'][0, 2])
+        self.assertFalse(graph['b'][0, 1])
+        self.assertFalse(graph['c'][0, 3])
 
 
 if __name__ == '__main__':

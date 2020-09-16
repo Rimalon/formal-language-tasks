64019cb67c8e32ec479e3b03c86ee870b48e9a4f
Rimalon <rimalon@yandex.ru>
2020-09-16 04:06:27 +0300

    Some dumb change for test working

--- src/tests/graphTest.py
+++ src/tests/graphTest.py
@@ -19,7 +19,7 @@ class GraphTestCase(unittest.TestCase):
         for label, matrix in graph.label_matrices.items():
             for i in range(matrix.nrows):
                 for j in range(matrix.ncols):
-                    self.assertEqual((i == 0 and j == 1 and label == 'a') or (i == 0 and j == 2 and label == 'b'), matrix[i, j])
+                    self.assertEqual((i == 0 and j == 1 and label == 'b') or (i == 0 and j == 2 and label == 'a'), matrix[i, j])
 
 
 if __name__ == '__main__':

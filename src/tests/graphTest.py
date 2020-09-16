8b67d0855f6d145de7e86e615baaba5d1d210ee9
Rimalon <rimalon@yandex.ru>
2020-09-16 03:48:29 +0300

    Fix bug

--- src/tests/graphTest.py
+++ src/tests/graphTest.py
@@ -12,29 +12,14 @@ class GraphTestCase(unittest.TestCase):
         file = open(regex_path)
         regex = Regex(file.readline())
         file.close()
-        from_regex = Graph.from_regex_file(regex_path)
-        print('from regex numeration dict')
-        for k, v in from_regex.vertice_numbering_dictionary.items():
-            print(k, ' ', v)
-        print('from regex graph:')
-        for label, matrix in from_regex.label_matrices.items():
-            for i in range(matrix.nrows):
-                for j in range(matrix.ncols):
-                    print(i, ' ', j, ' ', label, ' ', matrix[i, j])
-        result = from_regex.to_regex()
-        print(result)
+        result = Graph.from_regex_file(regex_path).to_regex()
         self.assertEqual(regex, result)
 
     def test_from_file(self):
         graph = Graph.from_file(test_resources_path + 'graph_a_or_b.txt')
-        print('from regex numeration dict')
-        for k, v in graph.vertice_numbering_dictionary.items():
-            print(k, ' ', v)
-        print('from regex graph:')
         for label, matrix in graph.label_matrices.items():
             for i in range(matrix.nrows):
                 for j in range(matrix.ncols):
-                    print(i, ' ', j, ' ', label, ' ', matrix[i,j])
                     self.assertEqual((i == 0 and j == 1 and label == 'a') or (i == 0 and j == 2 and label == 'b'), matrix[i, j])
 
 

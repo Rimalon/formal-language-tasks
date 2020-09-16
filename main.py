5b59cd0ac92f0f8a7e46769d638d9f7ac1b5cc72
Rimalon <rimalon@yandex.ru>
2020-09-16 15:26:16 +0300

    Fix tests run

--- main.py
+++ main.py
@@ -7,9 +7,9 @@ if __name__ == '__main__':
                         type=str, help='path to file with graph\nfile format:\n0 a 1\n0 b 2\n1 c 2')
     parser.add_argument('--query', required=True,
                         type=str, help='path to file with query\nfile format: regex in first line')
-    parser.add_argument('--fr', required=False,
+    parser.add_argument('--fr', required=False, default=None,
                         type=str, help='path to file with start vertices\nfile format:\n0 1 2')
-    parser.add_argument('--to', required=False,
+    parser.add_argument('--to', required=False, default=None,
                         type=str, help='path to file with end vertices\nfile format:\n0 1 2')
 
     print(parser.usage)

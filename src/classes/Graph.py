260e7dbb486cd11116f1a4fd4fa4e1d4181a269b
rimalon <rimalon@yandex.ru>
2020-09-23 06:23:32 +0300

    Change from dense to sparse

--- src/classes/Graph.py
+++ src/classes/Graph.py
@@ -13,7 +13,7 @@ class Graph:
 
     def __getitem__(self, item) -> Matrix:
         if item not in self.label_matrices.keys():
-            self.label_matrices[item] = Matrix.dense(BOOL, self.vertices_amount, self.vertices_amount)
+            self.label_matrices[item] = Matrix.sparse(BOOL, self.vertices_amount, self.vertices_amount)
         return self.label_matrices[item]
 
     def __and__(self, other):
@@ -52,6 +52,7 @@ class Graph:
 def from_file(path: str):
     file = open(path)
     transitions = file.read().split('\n')
+    transitions.remove('')
     file.close()
     result = Graph()
     i = 0
--- src/main/paths_query_executor.py
+++ src/main/paths_query_executor.py
@@ -20,14 +20,14 @@ def execute_query(args):
     query = Graph.from_regex_file(args.query)
 
     intersection = graph & query
-    intersection_matrix = Matrix.dense(BOOL, intersection.vertices_amount, intersection.vertices_amount)
+    intersection_matrix = Matrix.sparse(BOOL, intersection.vertices_amount, intersection.vertices_amount)
 
     for _, matrix in intersection.label_matrices.items():
         intersection_matrix += matrix
 
     closure = transitive_closure(intersection_matrix)
 
-    result = Matrix.dense(BOOL, graph.vertices_amount, graph.vertices_amount)
+    result = Matrix.sparse(BOOL, graph.vertices_amount, graph.vertices_amount)
     for i in range(closure.nrows):
         for j in range(closure.ncols):
             if (i in intersection.start_vertices) and (j in intersection.final_vertices) and (closure[i, j]):
@@ -37,7 +37,7 @@ def execute_query(args):
 
 
 def filter_query_result(matrix: Matrix, fr: set = None, to: set = None) -> Matrix:
-    result = Matrix.dense(BOOL, matrix.nrows, matrix.ncols)
+    result = Matrix.sparse(BOOL, matrix.nrows, matrix.ncols)
     for i in range(matrix.nrows):
         for j in range(matrix.ncols):
             if ((fr is None or (i in fr)) and matrix[i, j]) and ((to is None or (j in to)) and matrix[i, j]):

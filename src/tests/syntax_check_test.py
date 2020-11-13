4814ff0fe1c2bcbf7d878177e90973b4f9cacf22
rimalon <rimalon@yandex.ru>
2020-11-13 13:42:41 +0300

    Add syntax check execution from terminal

--- src/tests/syntax_check_test.py
+++ src/tests/syntax_check_test.py
@@ -13,10 +13,10 @@ syntax_grammar_path = project_path + 'syntax_grammar.txt'
 
 class SyntaxCheckTestCase(unittest.TestCase):
     def test_correct_syntax_success(self):
-        self.assertTrue(check_syntax(syntax_grammar_path, resources_path + 'script_correct_example.txt'))
+        self.assertTrue(check_syntax(resources_path + 'script_correct_example.txt', syntax_grammar_path))
 
     def test_incorrect_syntax_fail(self):
-        self.assertFalse(check_syntax(syntax_grammar_path, resources_path + 'script_incorrect_example.txt'))
+        self.assertFalse(check_syntax(resources_path + 'script_incorrect_example.txt', syntax_grammar_path))
 
     def test_correct_syntax(self):
         file = open(resources_path + 'syntax_expressions_data.txt')
--- syntactic_analyzer.py
+++ syntactic_analyzer.py
@@ -1,9 +1,15 @@
+import argparse
+import os
+
 from pyformlang.cfg import Terminal, CFG, Variable
 
 from src.classes.CNF import CNF
 from src.main.CYK import cyk
 
-def check_syntax(syntax_grammar_path:str, script_path: str):
+syntax_grammar_path_default = os.path.join(os.path.dirname(__file__), 'syntax_grammar.txt')
+
+
+def check_syntax(script_path: str, syntax_grammar_path: str = syntax_grammar_path_default):
     file = open(script_path)
     script_content = prepare_script(file.read())
     file.close()
@@ -15,3 +21,21 @@ def check_syntax(syntax_grammar_path:str, script_path: str):
 
 
 def prepare_script(script: str): return list(map(lambda c: Terminal(c), list(script.replace('\n', '').replace(' ', '#'))))
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--script', required=True,
+                        type=str, help='path to file with script.\nthe file format format depends on your syntax')
+    parser.add_argument('--syntax', required=False,
+                        type=str, help='path to file with syntax.\nfile format(pyformlang.cfg format):\nString -> Char String\nChar -> a | b\n')
+    args = parser.parse_args()
+    if args.syntax is None:
+        result = check_syntax(args.script)
+    else:
+        result = check_syntax(args.script, args.syntax)
+    if result:
+        print('Correct script')
+    else:
+        print('Incorrect script')
+

5aa593b6a0effc39e284595a58e88602910ee923
rimalon <rimalon@yandex.ru>
2020-11-13 05:05:57 +0300

    Add syntaxes and tests

---
+++ syntactic_analyzer.py
@@ -0,0 +1,20 @@
+from pyformlang.cfg import Terminal, CFG, Variable
+
+from src.classes.CNF import CNF
+from src.main.CYK import cyk
+
+syntax_grammar_path = '/home/rimalon/Desktop/formal-language-tasks/syntax_grammar.txt'
+
+
+def check_syntax(script_path: str):
+    file = open(script_path)
+    script_content = prepare_script(file.read())
+    file.close()
+    file = open(syntax_grammar_path)
+    syntax_grammar_text = file.read()
+    file.close()
+    syntax_grammar = CNF(CFG.from_text(syntax_grammar_text, Variable('Script')), True)
+    return cyk(script_content, syntax_grammar)
+
+
+def prepare_script(script: str): return list(map(lambda c: Terminal(c), list(script.replace('\n', '').replace(' ', '#'))))
